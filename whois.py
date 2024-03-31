30d4ede1ba7fc74d2636cb225be2b0a6ba34d613
TDBarnhouse <td.barnhouse@yahoo.com>
2024-03-31 18:19:42 -0400

    Updated messages

--- whois.py
+++ whois.py
@@ -1,11 +1,18 @@
 import socket
+import time
 
 def whois_lookup(domain: str):
+    print("Looking up target", domain + "...")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("whois.iana.org", 43))
     s.send(f"{domain}\r\n".encode())
     response = s.recv(4096).decode()
-    s.close
+    s.close()
+
+    if "returned 0 objects" in response:
+        print("Lookup failed: No results found.\n")
+    else:
+        print("Lookup completed.\n")
     return response
 
-print(whois_lookup("youtube.com"))
+print(whois_lookup("google.com"))
